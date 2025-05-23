785721e4e5df6257607cecc120fc7b0668b5b092
duonghieu <duonghieu0712z@gmail.com>
2025-05-23 16:46:39 +0700

    feat: disable context menu on prod

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ import { Main } from '@/views';
 
 export default function App() {
     return (
-        <div className='h-screen overflow-hidden'>
+        <div
+            className='h-screen overflow-hidden'
+            onContextMenu={(e) => {
+                if (import.meta.env.PROD) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+            }}
+        >
             <Main />
         </div>
     );

59e6b0a162941eb3895070393c18f5faed075fbe
duonghieu0712z <duonghieu0712z@gmail.com>
2026-05-29 13:58:45 +0700

    feat(layout): add theme mode toggle

--- src/layouts/MainContent.vue
+++ src/layouts/MainContent.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+import { Laptop2Icon, MoonIcon, SunIcon } from '@lucide/vue';
+import { useColorMode, useCycleList } from '@vueuse/core';
 import { storeToRefs } from 'pinia';
 
 import { useFeatures } from '@/composables/use-features';
 
 const featureStore = useFeatures();
 const { selectedGroup, selectedFeature } = storeToRefs(featureStore);
+
+const { store: themeStore } = useColorMode();
+const { state: themeState, next: nextTheme } = useCycleList(['auto', 'light', 'dark'] as const, {
+    initialValue: themeStore.value,
+});
+
+watchEffect(() => (themeStore.value = themeState.value));
 </script>
 
 <template>
@@ -18,6 +27,18 @@ const { selectedGroup, selectedFeature } = storeToRefs(featureStore);
                 <BreadcrumbItem>{{ selectedFeature.name }}</BreadcrumbItem>
             </BreadcrumbList>
         </Breadcrumb>
+
+        <Button
+            aria-label="Change color theme"
+            class="ml-auto"
+            size="icon"
+            variant="outline"
+            @click="nextTheme()"
+        >
+            <SunIcon v-if="themeStore === 'light'" />
+            <MoonIcon v-else-if="themeStore === 'dark'" />
+            <Laptop2Icon v-else />
+        </Button>
     </header>
 
     <div class="flex-1 px-12 py-8">

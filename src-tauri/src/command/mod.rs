cd746863a9d10a9fee40ba656f14841ca0c5e371
duonghieu0712z <duonghieu0712z@gmail.com>
2026-06-01 00:45:16 +0700

    chore: remove unused greet command

--- src-tauri/src/command/mod.rs
+++ src-tauri/src/command/mod.rs
@@ -2,12 +2,5 @@ use tauri::Runtime;
 use tauri_specta::{Commands, collect_commands};
 
 pub fn commands<R: Runtime>() -> Commands<R> {
-    collect_commands![greet]
-}
-
-#[tauri::command]
-#[specta::specta]
-fn greet(name: &str) -> String {
-    log::info!("Backend was called with an argument: {}", name);
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    collect_commands![]
 }
